0245cdbb74f860045b6250e22f4f531370dc2653
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-05-02 17:36:58 +0200

    Added scala class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     java
+    scala
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala-library:2.11.12")
 }
---
+++ src/main/scala/Mazzoli.scala
@@ -0,0 +1,8 @@
+object MazzoliScala {
+
+  def main(args: Array[String]): Unit = {
+    val m = new Mazzoli()
+    println("Used Mazzoli class")
+  }
+
+}
